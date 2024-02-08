9834c89d9ab32a9299e723ce87c82c8539d37c9d
ChristyBinu-4 <christy.binu91@gmail.com>
2024-02-08 12:26:01 +0530

    first commit

---
+++ Pythonfiles/getVariableName.py
@@ -0,0 +1,9 @@
+def get_variable_name(variable):
+    for name in globals():
+        if id(globals()[name]) == id(variable):
+            return name
+    for name in locals():
+        if id(locals()[name]) == id(variable):
+            return name
+    return None
+
